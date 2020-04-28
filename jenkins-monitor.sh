d47b1306a4fdb0a5f2200e8e527cb3a7e48271cc
kuanggang-git <62918167+kuanggang-git@users.noreply.github.com>
2020-04-28 15:03:19 +0800

    Update jenkins-monitor.sh

--- jenkins-monitor.sh
+++ jenkins-monitor.sh
@@ -4,4 +4,5 @@ export PATH
 c=$(ps -ef | grep "java -jar agent.jar"|grep -v grep | wc -l)
 if [ $c -eq 0 ];then
   service jenkins-slave restart
+  echo "Finished: service jenkins-slave restarted"
 fi
